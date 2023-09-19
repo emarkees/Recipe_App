94c5fe40593992094598549b726c2726aa76b362
emarkees <rphillipse@gmail.com>
2023-09-19 17:24:23 -0600

    Update recipe foods

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "recipes#index"
   # config/routes.rb
-
+  get '/public_recipes', to: 'recipes#public_recipes'
 get '/custom_login', to: 'sessions#new', as: 'custom_login'
 
   
