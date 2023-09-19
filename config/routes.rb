b07461952ebbcf1121a31a8efbb40a7e8e24881c
emarkees <rphillipse@gmail.com>
2023-09-19 15:50:54 -0600

    Update recipe routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "recipes#index"
   # config/routes.rb
 
 get '/custom_login', to: 'sessions#new', as: 'custom_login'
