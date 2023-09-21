045bc5ed59f527a2f8d46b0a763fc0a01d1dff91
emarkees <rphillipse@gmail.com>
2023-09-20 20:27:59 -0600

    Update Inventories and inventory_food views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_201656) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_022119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,15 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_201656) do
   create_table "inventory_foods", force: :cascade do |t|
     t.integer "quantity"
     t.bigint "inventory_id", null: false
-    t.bigint "food_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["food_id"], name: "index_inventory_foods_on_food_id"
-    t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
-  end
-
-  create_table "recipe_foods", force: :cascade do |t|
-    t.float "quantity"
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
